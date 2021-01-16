2181ebfed55c833b434ee518859253ee074af0f2
shubhsk88 <shubham88singh@gmail.com>
2021-01-16 17:27:35 +0530

    Adding the relationship with pizza

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -36,5 +36,27 @@ export default {
       description: 'Price of the pizza',
       validation: (Rule) => Rule.min(1000).max(5000),
     },
+    {
+      name: 'toppings',
+      type: 'array',
+      title: 'Toppings',
+      validation: (Rule) => Rule.unique(),
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const top = Object.values(toppings).filter(Boolean);
+
+      return { title, media, subtitle: top.join(', ') };
+    },
+  },
 };
