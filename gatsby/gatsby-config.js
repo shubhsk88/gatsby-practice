917af5c3d628c5c9c617cae9e6e902267f96ce2e
shubhsk88 <shubham88singh@gmail.com>
2021-01-19 05:55:29 +0530

    Fixing the bug

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,8 +1,24 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// This file is empty, but some people were reporting that it would not start unless they had an empty file.So here it is! You can delete the comment.Or replace it with your favourite shania twain lyrics.
 export default {
   siteMetadata: {
     title: 'Slick slice',
     sitUrl: 'http://gatsby.slice',
     description: 'The best pizza out in the lucknow ',
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'msiaxno3',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
