4af142318f34f54b2225b101a2d06819502947f7
shubhsk88 <shubham88singh@gmail.com>
2021-01-22 13:40:02 +0530

    Adding the styling for the pizza

--- gatsby/src/components/PizzasList.js
+++ gatsby/src/components/PizzasList.js
@@ -1,11 +1,30 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
 
+const StyledPizzaGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+`;
+const StyledPizza = styled.div`
+  @supports not (grid-template-row: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  display: grid;
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 function SinglePizza({ pizza }) {
   console.log(pizza);
   return (
-    <div>
+    <StyledPizza>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <h2>
           <span className="mark">{pizza.name}</span>
@@ -13,16 +32,16 @@ function SinglePizza({ pizza }) {
       </Link>
       <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
       <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
-    </div>
+    </StyledPizza>
   );
 }
 
 const PizzasList = ({ pizzas }) => (
-  <div>
+  <StyledPizzaGrid>
     {pizzas.map((pizza) => (
       <SinglePizza key={pizza.id} pizza={pizza} />
     ))}
-  </div>
+  </StyledPizzaGrid>
 );
 
 export default PizzasList;
